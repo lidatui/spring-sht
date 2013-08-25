f15f6c80d3a5923b2c7418b09bbcb4818cf30279
miemiedev <miemiedev@gmail.com>
2013-08-25 15:23:50 +0800

    init

--- src/main/java/com/github/miemiedev/sht/repo/UserDao.java
+++ src/main/java/com/github/miemiedev/sht/repo/UserDao.java
@@ -1,10 +1,11 @@
 package com.github.miemiedev.sht.repo;
 
 import com.github.miemiedev.sht.entity.User;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface UserDao extends PagingAndSortingRepository<User, Long> {
+public interface UserDao extends PagingAndSortingRepository<User, Long>,QueryDslPredicateExecutor<User> {
     List<User> findAll();
 }
--- src/main/java/com/github/miemiedev/sht/web/UserController.java
+++ src/main/java/com/github/miemiedev/sht/web/UserController.java
@@ -1,7 +1,11 @@
 package com.github.miemiedev.sht.web;
 
+import com.github.miemiedev.sht.entity.QUser;
+import com.github.miemiedev.sht.entity.User;
 import com.github.miemiedev.sht.repo.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,14 +14,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
  *
  */
 @Controller
-@RequestMapping(value = "/user")
 public class UserController {
     @Autowired
     private UserDao userDao;
 
-    @RequestMapping(value = "ha")
+    @RequestMapping(value = "/user")
     @ResponseBody
-    public Object ha() {
-        return userDao.findAll();
+    public Page ha() {
+        return userDao.findAll(QUser.user.name.like("%王%"), new PageRequest(0, 20));
     }
 }
